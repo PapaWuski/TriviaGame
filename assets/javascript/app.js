2e7cd716ca199508edc5bfe551e4090365b49dfa
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-09-09 09:11:10 -0700

    On click now loads questions + radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,86 @@
-const game = {
-    0:{
-        question:"what is jquery?",
-        choices:["libary","other answer", "choice3","module"],
-        answer:"libary"
-    }
-}
+const game = [
+  {
+    question: "what is jquery?",
+    choices: ["libary", "other answer", "choice3", "module"],
+    answer: "libary"
+  },
+  {
+    question: "what is jquery2?",
+    choices: ["libary", "other answer", "choice3", "module"],
+    answer: "libary"
+  }
+];
+var current = 0;
 
 const createStartPage = () => {
-  let $container = $(`.container`);
-  let $row = $(`<div>`).addClass(`row`);
-  let $col = $(`<div>`);
+  const $container = $(`.container`);
+  const $row = $(`<div>`).addClass(`row`);
+  const $col = $(`<div>`).addClass(`col-md-12 text-center`);
   let $title = $(`<h1>`);
   let $start = $(`<button>`);
 
   $($title).text("Trivia Game!");
   $($start).text("Start Quiz~");
+  $($start).addClass("quizStart");
 
-  $title = $row.clone().append(
-    $col
-      .clone()
-      .addClass(`col-md-12 text-center`)
-      .append($title)
-  );
-  $start = $row.clone().append(
-    $col
-      .clone()
-      .addClass(`col-md-12 text-center`)
-      .append($start)
-  );
+  $title = $row.clone().append($col.clone().append($title));
+  $start = $row.clone().append($col.clone().append($start));
 
   $container.append($title, $start);
+};
+const loadQuestion = () => {
+  const $container = $(`.container`);
+  const $row = $(`<div>`).addClass(`row`);
+  const $col = $(`<div>`).addClass(`col-md-12 text-center m-auto`);
+
+  game.forEach((question, questNum) => {
+    let $page = $row.clone().append($col.clone());
+    let $questionTitle = $row
+      .clone()
+      .addClass(`question`)
+      .append($col.clone().append($(`<h1>`).text(question.question)));
+    console.log($questionTitle.text());
+    let $answers = $col.clone();
+    question.choices.forEach(choice => {
+      let $label = $("<label>");
+      let $choice = $(`<input>`).attr({
+        name: `question${questNum}`,
+        value: choice,
+        type: "radio"
+      });
+      console.log("helloworld");
+      $answers.append($label.append($choice, choice));
+    });
+
+    $answers = $row
+      .clone()
+      .addClass(`answer`)
+      .append($answers);
+    $container.append(
+      $row.clone().append($col.clone().append($questionTitle, $answers))
+    );
+  });
+};
+
+const nextQuestion = () => {
+  loadQuestion();
+  current++;
+};
+const checkChoice = event => {
+  console.log(event);
+  const test = $(event.currentTarget);
+  console.log(test);
+  if ($(this).attr("data")) {
+    console.log("correct");
+  } else {
+    console.log("wrong");
+  }
+  if (current < game.length) {
+    nextQuestion();
+  }
 };
 
 $(document).ready(function() {
   createStartPage();
+  $(".quizStart").click(nextQuestion);
 });
